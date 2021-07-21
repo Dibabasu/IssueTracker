20188b37efd34b4ba88be9bf162ebfe1c94eaa96
Dibabasu Bhattacharya <dibbhatt@publicisgroupe.net>
2021-07-21 18:25:37 +0530

    Issue service init commit

---
+++ src/service/issuetracker/IssueTracker.Domain/Entities/IssueDetail.cs
@@ -0,0 +1,25 @@
+using System;
+using IssueTracker.Domain.Common;
+using IssueTracker.Domain.Enums;
+
+namespace IssueTracker.Domain.Entities
+{
+    public class IssueDetail : EntityBase
+    {
+        public string TicketNumber { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string CurrentStatus { get; set; }
+        public string UserName { get; set; }
+        public string Component { get; set; }
+        public string Catagory { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public string Type { get; set; }
+        public string SubType { get; set; }
+        public DateTime? ClosedAt { get; set; }
+        public string ClosedBy { get; set; }
+        public string ClosingRemarks { get; set; }
+
+
+    }
+}
