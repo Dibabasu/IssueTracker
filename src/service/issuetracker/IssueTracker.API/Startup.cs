da683e1fda6f118c0407cdcadebf9dd014b2a244
Dibabasu <dibabasub@winjit.com>
2021-08-15 23:03:40 +0530

    React UI Update

--- src/service/issuetracker/IssueTracker.API/Startup.cs
+++ src/service/issuetracker/IssueTracker.API/Startup.cs
@@ -21,9 +21,13 @@ namespace issueTracker.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
             services.AddApplicationServices();
 
             services.AddInfrastructureServices(Configuration);
+
+            services.AddCors();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -43,6 +47,12 @@ namespace issueTracker.API
             }
 
             app.UseRouting();
+
+            app.UseCors(x => x
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .SetIsOriginAllowed(origin => true) // allow any origin
+               .AllowCredentials()); // allow credentials
 
             app.UseAuthorization();
 
